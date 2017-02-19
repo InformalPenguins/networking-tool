95f8b315aca3ff5d2e2a0363b7111596d96ebd6a
Porfirio Partida <porfirio.partidai@gmail.com>
2017-02-19 01:22:53 -0700

    Lobby

---
+++ Assets/AdjustHeight.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustHeight : MonoBehaviour {
+    private int spacing;
+    RectTransform rectTransform;
+    private void Start()
+    {
+        VerticalLayoutGroup vlg = GetComponent<VerticalLayoutGroup>();
+        spacing = (int)vlg.spacing;
+        rectTransform = GetComponent<RectTransform>();
+    }
+    private void LateUpdate ()
+    {
+
+        int updateHeight = transform.childCount * spacing;
+        rectTransform.sizeDelta = new Vector2(0, updateHeight);
+    }
+}
