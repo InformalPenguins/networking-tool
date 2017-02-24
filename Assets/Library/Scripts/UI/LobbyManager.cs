3303ad85f7b2d68db4af3520c254b1de4a8fb855
Porfirio Partida <porfirio.partidai@gmail.com>
2017-02-24 00:20:40 -0700

    Fix light anim for button start, create rows on join pressed.

--- Assets/Library/Scripts/UI/LobbyManager.cs
+++ Assets/Library/Scripts/UI/LobbyManager.cs
@@ -13,18 +13,46 @@ public class LobbyManager : MonoBehaviour {
     public GameObject player1LobbyPrefab;
     public GameObject player2LobbyPrefab;
 
+    private const string TEAM_A_CONTAINER = "Canvas/Lobby/Teams/TeamA/Viewport/Content";
+    private const string TEAM_B_CONTAINER = "Canvas/Lobby/Teams/TeamB/Viewport/Content";
     public Scene PlayScene;
 
     private bool autoStart = false;
     private int startTime = 5;
 
+    private GameObject teamAGameObject, teamBGameObject;
     // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    void Start ()
+    {
+        teamAGameObject = GameObject.Find(TEAM_A_CONTAINER);
+        teamBGameObject = GameObject.Find(TEAM_B_CONTAINER);
+    }
+    public enum Team { A, B};
+    public void JoinClicked(Team team) {
+        switch (team) {
+            case Team.A:
+                GameObject newPlayerA = Instantiate(player1LobbyPrefab, teamAGameObject.transform, false);
+                newPlayerA.transform.SetAsFirstSibling();
+                //newPlayerA.transform.localScale = new Vector3(1, 1, 1);
+                break;
+            case Team.B:
+                GameObject newPlayerB = Instantiate(player2LobbyPrefab,teamBGameObject.transform, false);
+                newPlayerB.transform.SetAsLastSibling();
+                //newPlayerB.transform.localScale = new Vector3(1, 1, 1);
+                break;
+        }
+    }
+    public void JoinAClicked()
+    {
+        JoinClicked(Team.A);
+    }
+    public void JoinBClicked()
+    {
+        JoinClicked(Team.B);
+    }
+
+        // Update is called once per frame
+        void Update () {
 		
 	}
 }
